afa99298f556997d88ce81bc269472f184645e68
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-09-11 13:36:08 +0800

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,14 @@ import MainLayout from '@/components/layouts/mainLayout';
 
 export default function Home() {
   return (
-    export default function Home() {
-      return (
-        <MainLayout title="Welcome Homepage">
-          <div
-            className="grid min-h-[100vh] w-full place-items-center bg-light bg-cover bg-no-repeat"
-            style={{
-              backgroundImage:
-                'url(https://web.bookandlink.com/wp-content/uploads/2020/04/banner-image-bookandlink3.jpg)',
-            }}
-          >
-          </div>
+    <MainLayout title="Welcome Homepage">
+      <div
+        className="grid min-h-[100vh] w-full place-items-center bg-light bg-cover bg-no-repeat"
+        style={{
+          backgroundImage:
+            'url(https://web.bookandlink.com/wp-content/uploads/2020/04/banner-image-bookandlink3.jpg)',
+        }}
+      >
         <div>Slider</div>
       </div>
       <div className="min-h-[400px] bg-blue-200">section 2</div>
