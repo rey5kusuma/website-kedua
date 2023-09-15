5bb4ad648cdb045e32bdc85b8ccef483bdf091e6
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-09-15 15:45:10 +0800

    1 file modified

--- pages/contact.js
+++ pages/contact.js
@@ -62,7 +62,7 @@ export default function Contact() {
               </div>
             </div>
           </div>
-          <div className="w-96 ml-14 my-10 flex flex-row">
+          <div className="w-96 ml-10 my-10 flex flex-row">
             <div className="border border-y-0 border-l-0 px-4 pt-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -75,8 +75,41 @@ export default function Contact() {
                 <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
               </svg>
             </div>
+            <div className="ml-4">
+              <div className="text-gray-100">
+                <h1 className="text-xl font-semibold">ALAMAT</h1>
+                <p className="text-xs mt-2 font-semibold">GENERAL INQUIRIES</p>
+                <Link href="#">
+                  <p className="text-xs mt-1">sales@bookandlink.com</p>
+                </Link>
+                <p className="text-xs mt-2 font-semibold">INDONESIA</p>
+                <Link href="#">
+                  <p className="text-xs mt-1">indonesia@bookandlink.com</p>
+                </Link>
+                <p className="text-xs mt-2 font-semibold">MALAYSIA</p>
+                <Link href="#">
+                  <p className="text-xs mt-1">malaysia@bookandlink.com</p>
+                </Link>
+                <p className="text-xs mt-2 font-semibold">VIETNAM</p>
+                <Link href="#">
+                  <p className="text-xs mt-1">vietnam@bookandlink.com</p>
+                </Link>
+                <p className="text-xs mt-2 font-semibold">THAILAND</p>
+                <Link href="#">
+                  <p className="text-xs mt-1">thailand@bookandlink.com</p>
+                </Link>
+                <p className="text-xs mt-2 font-semibold">PHILIPPINES</p>
+                <Link href="#">
+                  <p className="text-xs mt-1">philippines@bookandlink.com</p>
+                </Link>
+                <p className="text-xs mt-2 font-semibold">USA</p>
+                <Link href="#">
+                  <p className="text-xs mt-1">usa@bookandlink.com</p>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="w-96 ml-14 my-10 flex flex-row">
+          <div className="w-96 ml-2 my-10 flex flex-row">
             <div className="border border-y-0 border-l-0 px-4 pt-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -89,6 +122,19 @@ export default function Contact() {
                 <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
                 <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
               </svg>
+            </div>
+            <div className="ml-4">
+              <div className="text-gray-100">
+                <h1 className="text-xl font-semibold">ALAMAT</h1>
+                <p className="text-xs mt-2 font-semibold">General Inquiries</p>
+                <p className="text-xs mt-1">+62(0)361 8499 358 (OFFICE)</p>
+                <p className="text-xs mt-2 font-semibold">Sales</p>
+                <p className="text-xs mt-1">+62(0)8 11 3856 575 (WA)</p>
+                <p className="text-xs mt-2 font-semibold">
+                  Business Development and Partnership
+                </p>
+                <p className="text-xs mt-1">+62(0)8 11 3945 459 (WA)</p>
+              </div>
             </div>
           </div>
         </div>
