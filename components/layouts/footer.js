ee1f0dc30d77dd5530ef9469e533fe34b571cf3d
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-09-15 11:30:20 +0800

    1 file modified

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -1,9 +1,101 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <footer className="min-h-[300px] bg-black flex flex-row">
-      <div className="border ml-10 my-4 w-80"></div>
-      <div className="border ml-10 my-4 w-80"></div>
-      <div className="border ml-10 my-4 w-full"></div>
+    <footer className="min-h-[240px] bg-black flex flex-row">
+      <div className="ml-14 my-4" style={{ width: '350px' }}>
+        <h1 className="text-white text-lg mt-6 ml-8">HALAMAN</h1>
+        <div className="flex flex-row mx-8 mt-4">
+          <div className="text-sm min-h-[120px]">
+            <p className="text-white">
+              <Link href="#">Beranda</Link>
+            </p>
+            <p className="text-white mt-2">
+              <Link href="#">Produk</Link>
+            </p>
+            <p className="text-white mt-2">
+              <Link href="#">Mitra</Link>
+            </p>
+            <p className="text-white mt-2">
+              <Link href="#">Servis</Link>
+            </p>
+          </div>
+          <div className="text-sm ml-4 min-h-[120px]">
+            <p className="text-white">
+              <Link href="#">Harga</Link>
+            </p>
+            <p className="text-white mt-2">
+              <Link href="#">Tentang Kami</Link>
+            </p>
+            <p className="text-white mt-2">
+              <Link href="#">Kontak</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="ml-6 my-4" style={{ width: '400px' }}>
+        <h1 className="text-white text-lg mt-6 ml-8">KONTAK</h1>
+        <div className="mx-8 mt-4">
+          <div className="text-sm flex flex-row place-items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="white"
+              class="bi bi-telephone-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+              />
+            </svg>
+            <p className="text-white ">
+              <Link href="#">+62 361 8499 358 (KANTOR)</Link>
+            </p>
+          </div>
+          <div className="text-sm flex flex-row place-items-center mt-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="white"
+              class="bi bi-whatsapp"
+              viewBox="0 0 16 16"
+            >
+              <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+            </svg>
+            <p className="text-white ml-3">
+              <Link href="#">+62 811 3945 459 (WA)</Link>
+            </p>
+          </div>
+          <div className="text-sm flex flex-row place-items-center mt-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="white"
+              class="bi bi-envelope"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+            </svg>
+            <p className="text-white  ml-3">
+              <Link href="#">contact@bookandlink.com</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="border ml-8 my-4 mr-4" style={{ width: '440px' }}>
+        <div className="flex flex-row">
+          <img
+            src="https://web.bookandlink.com/wp-content/uploads/2020/04/logo-footer.png"
+            className="h-8 mt-5"
+            alt="bookandlink Logo"
+            style={{ height: '80px' }}
+          />
+        </div>
+      </div>
     </footer>
   );
 }
