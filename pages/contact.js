7a14bf50f56152f39e6e01c11c5ab336ee9476c0
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-09-20 11:14:16 +0800

    1 file modified

--- pages/contact.js
+++ pages/contact.js
@@ -234,6 +234,14 @@ export default function Contact() {
             <div className="bg-gray-200 w-72 ml-6 p-2 mt-12 h-20">
               <p className="ml-10 mt-5">Saya bukan robot</p>
             </div>
+            <div className="ml-6 mt-6">
+              <button
+                type="button"
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-lg px-12 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                KIRIM
+              </button>
+            </div>
           </div>
         </div>
       </div>
