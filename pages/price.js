df45d23bc0c543ebe19d3674c3bb0d4d3decc0b8
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-10-05 14:05:18 +0800

    1 file modified

--- pages/price.js
+++ pages/price.js
@@ -4,7 +4,20 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <MainLayout title="Welcome Homepage">
-      <div className="grid min-h-[230vh]"></div>
+      <div className="grid min-h-[230vh]">
+        <div className="">
+          <h1>SIMPLIFY OPERATIONS TO MAXIMIZE YOUR REVENUE</h1>
+          <p>Find the perfect plan for your property</p>
+          <h1>REQUEST FOR A QUOTE</h1>
+          <p>
+            Every properties are unique, and each has different sets of
+            requirements. Therefore, with our modular pricing strategy, we
+            provide you with a personalized quote, taking into account the
+            number of rooms, modules, and interfaces. Complete the form below to
+            get the most beneficial pricing plan for your hotel.
+          </p>
+        </div>
+      </div>
     </MainLayout>
   );
 }
