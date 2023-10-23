42ba3652b9d401ee39ffc6cf8d968c9cd617c498
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-10-23 12:03:34 +0800

    1 file modified

--- pages/partner.js
+++ pages/partner.js
@@ -13,11 +13,35 @@ export default function Partner() {
             style={{ height: '300px' }}
           />
         </div>
-        <div className="border border-black h-[180vh] mx-16">
+        <div className="border border-black h-[180vh] mx-14">
           <h1 className="text-4xl text-center text-gray-700 mt-8">
             Mitra BookandLink
           </h1>
-          <div className="border border-black h-[50px] mt-14 bg-sky-600"></div>
+          <div className="border border-black flex h-[45px] mt-14 bg-sky-500 text-sm">
+            <div className="px-8 py-2 bg-yellow-500">OTA</div>
+            <div className="px-8 py-2">WHOLESALER</div>
+            <div className="px-8 py-2">PMS</div>
+            <div className="px-8 py-2">BOOKING ENGINE</div>
+            <div className="px-8 py-2">PAYMENT GATEWAY</div>
+            <div className="px-8 py-2">ICALENDAR SYNC</div>
+            <div className="px-8 py-2">METASEARCH</div>
+            <div className="px-8 py-2">RMS</div>
+            <div className="px-8 py-2">HOTEL APP</div>
+          </div>
+          <div className="text-gray-700 text-center mt-10">
+            <h1>Agen Perjalanan Online (OTA)</h1>
+            <p>
+              OTA adalah singkatan Agen Perjalanan Online. OTA adalah perusahaan
+              online yang situs webnya memungkinkan konsumen memesan berbagai
+              layanan terkait perjalanan langsung melalui Internet. Mereka
+              adalah agen pihak ke-3 yang menjual kembali perjalanan, hotel,
+              mobil, penerbangan, paket liburan, dan lain-lain yang diisediakan
+              atau diorganisir oleh orang lain.
+            </p>
+          </div>
+          <div className="flex">
+            <div className="border border-black w-56"></div>
+          </div>
         </div>
       </div>
     </MainLayout>
