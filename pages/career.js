c7bba58c739f61016e911f45cfdc64f768e01554
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-10-26 09:01:17 +0800

    1 file modified

--- pages/career.js
+++ pages/career.js
@@ -4,12 +4,18 @@ import Link from 'next/link';
 export default function Affiliate() {
   return (
     <MainLayout title="Welcome Homepage">
-      <div className="min-h-[210vh]">
-        <div className="h-[60vh] mt-16">
+      <div className="min-h-[85vh] bg-blue-100">
+        <div className="mt-16">
           <img
             src="https://web.bookandlink.com/wp-content/uploads/2020/05/main-hiring-1500x323.jpg"
             style={{ height: '300px' }}
           />
+        </div>
+        <div className="text-center text-gray-600">
+          <h1 className="text-4xl mt-14 mb-6">Lowongan Pekerjaan</h1>
+          <p className="text-lg">
+            Maaf, saat ini tidak ada posisi yang tersedia.
+          </p>
         </div>
       </div>
     </MainLayout>
