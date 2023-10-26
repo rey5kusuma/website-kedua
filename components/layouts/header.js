092b4380c15b16f26db13cf28d197d334f43c6a8
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-10-26 14:24:16 +0800

    2 files modified

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -20,7 +20,7 @@ export default function Header() {
             <ul className="font-sm text-sm flex items-center flex-col p-4 md:p-0 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
               <li>
                 <Link
-                  href="#"
+                  href="login"
                   class="block py-2 px-2 ml-32 text-gray-900 font-semibold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Akses Klien
--- pages/login.js
+++ pages/login.js
@@ -2,5 +2,43 @@ import MainLayout from '@/components/layouts/mainLayout';
 import Link from 'next/link';
 
 export default function Service() {
-  return <div></div>;
+  return (
+    <div
+      className="grid min-h-[110vh] w-full place-items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage:
+          'url(https://www.bookandlink.com/admin/assets/img/bg-home.jpg)',
+      }}
+    >
+      <div className="h-[500px] w-[370px] bg-gray-50">
+        <div className="border border-black grid justify-center items-center w-[280px]">
+          <img
+            src="https://www.bookandlink.com/admin/assets/images/channelku3.png"
+            alt=""
+          />
+        </div>
+        <div className="border border-black mx-5 mt-4">
+          <h1 className="text-center text-gray-700 text-[15px]">
+            Log in to start your session
+          </h1>
+          <div className="mt-4">
+            <input
+              type="text"
+              id="small-input"
+              className="block w-full p-2 text-gray-900 border border-gray-400 rounded-sm bg-gray-50 text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div className="mt-4">
+            <input
+              type="text"
+              id="small-input"
+              className="block w-full p-2 text-gray-900 border border-gray-400 rounded-sm bg-gray-50 text-sm"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
