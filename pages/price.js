f01abb996e669db7fc85184797e0528d678f7fba
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-10-27 11:01:11 +0800

    1 file modified

--- pages/price.js
+++ pages/price.js
@@ -5,15 +5,17 @@ export default function Home() {
   return (
     <MainLayout title="Free Trial - Cloud Hospitality Software Indonesia">
       <div className="h-[230vh] bg-blue-400">
-        <div className="border grid h-[38vh] mt-32 mx-16 text-center">
+        <div className="grid h-[38vh] mt-32 mx-16 text-center">
           <h1 className="text-3xl mt-8 text-gray-700">
             SIMPLIFY OPERATIONS TO MAXIMIZE YOUR REVENUE
           </h1>
-          <p className="text-2xl mt-2 text-gray-700">
+          <p className="text-2xl mt-2 text-gray-700 text-gray-700">
             Find the perfect plan for your property
           </p>
-          <h1 className="mt-2 text-4xl font-semibold">REQUEST FOR A QUOTE</h1>
-          <p className="mt-3 px-2 rounded text-white text-lg bg-sky-950">
+          <h1 className="mt-2 text-4xl font-semibold text-gray-700">
+            REQUEST FOR A QUOTE
+          </h1>
+          <p className="mt-3 px-2 rounded text-white text-[17px] bg-sky-950">
             Every properties are unique, and each has different sets of
             requirements. Therefore, with our modular pricing strategy, we
             provide you with a personalized quote, taking into account the
@@ -21,7 +23,105 @@ export default function Home() {
             get the most beneficial pricing plan for your hotel.
           </p>
         </div>
-        <div className="h-[100vh] mx-16 mt-4 bg-blue-200"></div>
+        <div className="grid h-[110vh] mx-16 mt-4 bg-blue-100">
+          <div className="border border-black mx-6 mt-4 h-[620px] text-gray-700">
+            <h1 className="text-xl font-bold">Request For a Quote</h1>
+            <div className="flex border border-black mt-4">
+              <div className="w-full">
+                <label
+                  for="first_name"
+                  classNanme="block mb-2 text-[16px] text-gray-900"
+                >
+                  Your Name *
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="mt-2 bg-blue-100 border border-gray-400 border-2 text-gray-900 text-sm rounded-lg block w-full p-1.5 "
+                  required
+                />
+              </div>
+              <div className="w-full ml-4">
+                <label
+                  for="first_name"
+                  classNanme="block mb-2 text-[16px] text-gray-900"
+                >
+                  E-Mail *
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="mt-2 bg-blue-100 border border-gray-400 border-2 text-gray-900 text-sm rounded-lg block w-full p-1.5 "
+                  required
+                />
+              </div>
+            </div>
+            <div className="flex border border-black mt-4">
+              <div className="w-full">
+                <label
+                  for="first_name"
+                  classNanme="block mb-2 text-[16px] text-gray-900"
+                >
+                  Hotel Name
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="mt-2 bg-blue-100 border border-gray-400 border-2 text-gray-900 text-sm rounded-lg block w-full p-1.5 "
+                  required
+                />
+              </div>
+              <div className="w-full ml-4">
+                <label
+                  for="first_name"
+                  classNanme="block mb-2 text-[16px] text-gray-900"
+                >
+                  Phone Number *
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="mt-2 bg-blue-100 border border-gray-400 border-2 text-gray-900 text-sm rounded-lg block w-full p-1.5 "
+                  required
+                />
+              </div>
+              <div className="w-full ml-4">
+                <label
+                  for="first_name"
+                  classNanme="block mb-2 text-[16px] text-gray-900"
+                >
+                  Number of Rooms/Units
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="mt-2 bg-blue-100 border border-gray-400 border-2 text-gray-900 text-sm rounded-lg block w-full p-1.5 "
+                  required
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label for="message" class="block mb-2 text-[16px] text-gray-700">
+                Message
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                class="block p-2.5 w-full h-52 text-sm text-gray-900 bg-blue-100 rounded-lg border border-gray-400 border-2"
+              ></textarea>
+            </div>
+          </div>
+          <div className="">
+            <div className="">
+              <button
+                type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Default
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </MainLayout>
   );
