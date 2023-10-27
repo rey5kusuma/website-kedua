8c58f9b3197ca73909981799dbd6c9c41d417925
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-10-27 12:25:34 +0800

    1 file modified

--- pages/price.js
+++ pages/price.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <MainLayout title="Free Trial - Cloud Hospitality Software Indonesia">
-      <div className="h-[230vh] bg-blue-400">
+      <div className="h-[233vh] bg-blue-400">
         <div className="grid h-[38vh] mt-32 mx-16 text-center">
           <h1 className="text-3xl mt-8 text-gray-700">
             SIMPLIFY OPERATIONS TO MAXIMIZE YOUR REVENUE
@@ -122,11 +122,212 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="grid h-[70vh] bg-blue-100 mt-16">
-          <div className="border border-black w-full flex">
-            <div className="w-full border border-black"></div>
-          </div>
-          <div className="w-full border border-black"></div>
+        <div className="grid h-[72vh] bg-blue-100 mt-16">
+          <div className="w-full flex">
+            <div className="w-full border border-white border-1">
+              <div className="mt-20 ml-14 h-[280px]">
+                <h1 className="text-3xl text-gray-700">ADD ONS</h1>
+                <div className="flex">
+                  <div className="ml-4 text-gray-700">
+                    <div className="flex items-center mt-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="6"
+                        height="6"
+                        fill="currentColor"
+                        class="bi bi-circle-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <circle cx="8" cy="8" r="8" />
+                      </svg>
+                      <div className="ml-4">POS (Point of Sale)</div>
+                    </div>
+                    <div className="flex items-center mt-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="6"
+                        height="6"
+                        fill="currentColor"
+                        class="bi bi-circle-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <circle cx="8" cy="8" r="8" />
+                      </svg>
+                      <div className="ml-4">RATE SHOPPER</div>
+                    </div>
+                    <div className="flex items-center mt-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="6"
+                        height="6"
+                        fill="currentColor"
+                        class="bi bi-circle-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <circle cx="8" cy="8" r="8" />
+                      </svg>
+                      <div className="ml-4">DYNAMIC PRICING</div>
+                    </div>
+                    <div className="flex items-center mt-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="6"
+                        height="6"
+                        fill="currentColor"
+                        class="bi bi-circle-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <circle cx="8" cy="8" r="8" />
+                      </svg>
+                      <div className="ml-4">LOYALTY PROGRAM</div>
+                    </div>
+                    <div className="flex items-center mt-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="6"
+                        height="6"
+                        fill="currentColor"
+                        class="bi bi-circle-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <circle cx="8" cy="8" r="8" />
+                      </svg>
+                      <div className="ml-4">BACK OFFICE SYSTEM</div>
+                    </div>
+                  </div>
+                  <div className="mt-4 ml-16">
+                    <div>
+                      <button
+                        type="button"
+                        class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1.5 mr-2 mb-2"
+                      >
+                        CONTACT US
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        type="button"
+                        class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1.5 mr-2 mb-2"
+                      >
+                        CONTACT US
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        type="button"
+                        class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1.5 mr-2 mb-2"
+                      >
+                        CONTACT US
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        type="button"
+                        class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1.5 mr-2 mb-2"
+                      >
+                        CONTACT US
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        type="button"
+                        class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1.5 mr-2 mb-2"
+                      >
+                        CONTACT US
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full border border-white border-1">
+              <div className="mt-20 ml-14 h-[280px]">
+                <h1 className="text-3xl text-gray-700">INFORMATION</h1>
+                <div className="ml-4 text-gray-700">
+                  <div className="flex items-center mt-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="6"
+                      height="6"
+                      fill="currentColor"
+                      class="bi bi-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <circle cx="8" cy="8" r="8" />
+                    </svg>
+                    <div className="ml-4">1 MONTH FREE TRIAL</div>
+                  </div>
+                  <div className="flex items-center mt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="6"
+                      height="6"
+                      fill="currentColor"
+                      class="bi bi-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <circle cx="8" cy="8" r="8" />
+                    </svg>
+                    <div className="ml-4">1 YEAR PAYMENT = 2 MONTH FREE</div>
+                  </div>
+                  <div className="flex items-center mt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="6"
+                      height="6"
+                      fill="currentColor"
+                      class="bi bi-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <circle cx="8" cy="8" r="8" />
+                    </svg>
+                    <div className="ml-4">NO SETUP FEES</div>
+                  </div>
+                  <div className="flex items-center mt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="6"
+                      height="6"
+                      fill="currentColor"
+                      class="bi bi-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <circle cx="8" cy="8" r="8" />
+                    </svg>
+                    <div className="ml-4">NO CANCELLATION PENALTIES</div>
+                  </div>
+                  <div className="flex items-center mt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="6"
+                      height="6"
+                      fill="currentColor"
+                      class="bi bi-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <circle cx="8" cy="8" r="8" />
+                    </svg>
+                    <div className="ml-4">
+                      TRAINING IN OUR OFFICE / AT LOCATION (WHEN POSSIBLE)/
+                      ONLINE
+                    </div>
+                  </div>
+                  <div className="flex items-center mt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="6"
+                      height="6"
+                      fill="currentColor"
+                      class="bi bi-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <circle cx="8" cy="8" r="8" />
+                    </svg>
+                    <div className="ml-4">SUPPORT VIA EMAIL/WHATSAPP</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
